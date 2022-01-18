55525f22106bdfbe8ecf5a7613abac496e7210b7
team <team@Pablos-iMac.local>
2022-01-18 13:51:54 -0700

    Slider structure

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import { relative } from "path";
+import React from "react";
+import Container from "./Container";
 
 const Slider = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { className } = props;
+  const [bgColor, setBgColor] = React.useState("top");
   return (
-    <button
-      {...props}
-       //className={`rounded-xl bg-blue-600 text-white py-3 px-4 uppercase ${className}`}
-    ></button>
+    <div>
+      <div>
+        <div
+          style={{
+            width: 90,
+            maxWidth: 900,
+            margin: "auto",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        ></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div
+          className="bg-black bg-opacity-50"
+          style={{
+            position: "absolute",
+            width: 100,
+            height: 100,
+            top: 0,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            fontSize: 20,
+          }}
+        >
+          &#60;
+        </div>{" "}
+        //botones
+        <div
+          className="bg-black bg-opacity-50"
+          style={{ position: "absolute", width: 40, height: 40 }}
+        >
+          &#62;
+        </div>
+      </div>
+    </div>
   );
 };
 
--- pages/media.tsx
+++ pages/media.tsx
@@ -1,15 +1,17 @@
-import Layout from '@/components/Layout';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Slider from "@/components/Slider";
 
-import MediaSection from '@/sections/MediaSection';
-import VideosSection from '@/sections/VideosSection';
-import LatestNewsSection from '@/sections/LatestNewsSection';
+import MediaSection from "@/sections/MediaSection";
+import VideosSection from "@/sections/VideosSection";
+import LatestNewsSection from "@/sections/LatestNewsSection";
 
 const Media = () => {
   return (
     <Layout>
       <Navbar />
+      <Slider />
       <MediaSection />
       {/* <VideosSection />
       <LatestNewsSection /> */}
