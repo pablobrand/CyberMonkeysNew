e0232fa3ed6058b0bc8d6405005616be4f5a96b4
team <team@Pablos-iMac.local>
2022-01-26 09:17:50 -0700

    Slider video

--- sections/BannerSection.tsx
+++ sections/BannerSection.tsx
@@ -5,15 +5,7 @@ import Container from "@/components/Container";
 const BannersSection = () => {
   return (
     <section>
-      <Container>
-        <div className="text-center mb-2">
-          <Image
-            src="/images/audiovisualProduction.png"
-            width={2448}
-            height={607}
-          />
-        </div>
-      </Container>
+      
     </section>
   );
 };
