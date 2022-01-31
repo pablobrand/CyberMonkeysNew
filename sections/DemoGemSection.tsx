09454c31ec42e3a40ed44fd8f38699cd24003d3d
pablobrand <pabrandenburg@gmail.com>
2022-01-30 19:00:25 -0700

    updated demo page for sales pitch

--- sections/DemoGemSection.tsx
+++ sections/DemoGemSection.tsx
@@ -11,34 +11,41 @@ const DemoGemSection = () => {
         <section className="grid min-h-screen mt-20 mb-24 text-white">
             <Container>
                 <div className="col-span-12 lg:col-span-5">
-                    <h1 className="text-5xl pt-28 text-center pb-4">
+                    <h1 className="text-5xl pt-28 text-center">
                         <span className="block">
                             This is a demo for Gem Stones Image and Augemented Reality ecommerce
                             page
                         </span>
                     </h1>
                 </div>
-                <div className="md:container md:mx-auto">
+                <div className="md:container md:mx-auto pb-12 pt-32">
                     <VideoSlider slides={SliderData} />
                 </div>
-
-                <div className="grid grid-cols-12">
-                    <div className="col-span-12 lg:col-span-5">
-                        <h1 className="text-5xl pt-32">
-                            <span className="block">This is my DemoGemSection</span>
-                            <span className="block font-semibold">Cybermonkeys LLC</span>
+            </Container>
+            <Container>
+                <div className="grid grid-cols-10">
+                    <div className="col-span-12 lg:col-span-5  text-center">
+                        <h1 className="text-4xl pt-24">
+                            <span className="block">We canned a real Rock into a 3D model, that buyers and see in front of them.</span>
+                            <span className="block font-semibold">Scan to see in AR</span>
                         </h1>
-                        <p className="text-xl font-medium mb-8">bla</p>
+                        <Image src="/images/RockModelQRCode.png" width={540} height={540} />
                     </div>
-                    <div className="col-span-12 lg:col-span-7 text-right">
-                        <div className="pt-16">
-                            <Image src="/images/media.png" width={540} height={432} />
+                    <div className="col-span-12 lg:col-span-5 text-center">
+                        <h1 className="text-4xl pt-24">
+                            <span className="block">Below is the image on in 3D, which is then pass to AR tool.</span>
+                        </h1>
+                        <div className="pt-2">
+                            <Image src="/images/Rockin3dAR.png" width={540} height={432} />
                         </div>
-                        <Link href="/">
-                            <a className="uppercase bg-blue-600 px-6 py-3 rounded-full">
-                                Buy Now
-                            </a>
-                        </Link>
+                        <div className="pt-8">
+                            <Link href="/">
+                                <a className="uppercase bg-blue-600 px-6 py-3 rounded-full text-xl">
+                                    Buy Now
+                                </a>
+                            </Link>
+                        </div>
+                        
                     </div>
                 </div>
             </Container>
