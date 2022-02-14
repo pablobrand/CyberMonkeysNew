32c22b44c40ecaa38e171b318177dfa671c6045b
team <team@Pablos-iMac.local>
2022-02-14 10:18:58 -0700

    initial commit

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer/Footer";
+import VideoSlider from "@/components/Slider/VideoSlider";
+//import { SliderData } from "@/components/Slider/SliderData.js";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <Navbar />
+      {/* <HomeSection /> */}
+      {/* <GlobeSection />
+      <ServicesSection /> */}
+      {/* <WhyChooseUsSection />
+      <FAQSection />
+      <LatestNewsSection /> */}
+      <Footer />
+    </Layout>
+  );
+};
+
+export default LoginPage;
