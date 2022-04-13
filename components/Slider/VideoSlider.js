3575e7138f54ab6ead6e60d4b1e9f38fab5b3ec8
Pablo <pabrandenburg@gmail.com>
2022-04-13 12:26:33 -0700

    removed slider and added image below

--- components/Slider/VideoSlider.js
+++ components/Slider/VideoSlider.js
@@ -1,102 +1,67 @@
-import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
-import React, { useState, useRef } from "react";
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-import { Video, Image, CloudinaryContext, Transformation } from "cloudinary-react";
+import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event"
+import React, { useState, useRef } from "react"
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa"
+import {
+  Video,
+  Image,
+  CloudinaryContext,
+  Transformation,
+} from "cloudinary-react"
 
 const VideoSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
+  const [current, setCurrent] = useState(0)
+  const length = 2
 
   //controla la posicion de los slides
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
 
   //if no slides no muestra nada
   if (!Array.isArray(slides) || slides.length <= 0) {
-    return null;
+    return null
   }
 
   const switchingElements = (slide) => {
-    //console.log("current number: " + current);
-    //console.log("slides value: " + JSON.stringify(slide));
-    const videoRef = useRef();
+    const videoRef = useRef()
     if (current === 0) {
       return (
         <>
-           <CloudinaryContext cloud_name="cybermonkeysllc">
+          <CloudinaryContext cloud_name="cybermonkeysllc">
             <div>
-                <Video
-                    publicId="CyberMonkeys/B1_zdvhe0"
-                    autoplay = "true"
-                    sound = "false"
-                    loop = "true"
-                    className="w-screen"
-                    innerRef={videoRef}>
-                      <Transformation effect="fade:500"/>
-
-                </Video>
+              <Video
+                publicId="CyberMonkeys/B1_zdvhe0"
+                autoplay="true"
+                sound="false"
+                loop="true"
+                className="w-screen"
+                innerRef={videoRef}
+              >
+                <Transformation effect="fade:500" />
+              </Video>
             </div>
-        </CloudinaryContext>
+          </CloudinaryContext>
         </>
-      );
-    } 
-    if (current === 1){
+      )
+    } else {
       return (
         <>
-        <CloudinaryContext cloud_name="cybermonkeysllc">
+          <CloudinaryContext cloud_name="cybermonkeysllc">
             <div>
-                <Image
-                    publicId="CyberMonkeys/20100925_WBP504_ft3ywl.webp"
-                    className="w-screen"
-                    innerRef={videoRef}>
-
-                </Image>
+              <Image
+                publicId="CyberMonkeys/SLIDER_PRINCIPAL_JPG_ejwrkr.jpg"
+                innerRef={videoRef}
+              ></Image>
             </div>
-        </CloudinaryContext>
-         
+          </CloudinaryContext>
         </>
-      );
+      )
     }
-    if (current === 2){
-      return (
-        <>
-        <CloudinaryContext cloud_name="cybermonkeysllc">
-            <div>
-                <Image
-                    publicId="CyberMonkeys/gautier-salles-uffQnKuJ-hc-unsplash_fl7nsn.jpg"
-                    className="w-screen"
-                    innerRef={videoRef}>
-
-                </Image>
-            </div>
-        </CloudinaryContext>
-         
-        </>
-      );
-    }
-    else {
-      return (
-        <>
-        <CloudinaryContext cloud_name="cybermonkeysllc">
-            <div>
-                <Image
-                    publicId="CyberMonkeys/robert-murray-toCqTyxsT4Q-unsplash_qbofl1.jpg"
-                    className="w-screen"
-                    innerRef={videoRef}>
-
-                </Image>
-            </div>
-        </CloudinaryContext>
-         
-        </>
-      );
-    }
-  };
+  }
   //"FaAngle" are icons
   return (
     <section className="slider">
@@ -110,10 +75,10 @@ const VideoSlider = ({ slides }) => {
           >
             {index === current && switchingElements(slide)}
           </div>
-        );
+        )
       })}
     </section>
-  );
-};
+  )
+}
 
-export default VideoSlider;
+export default VideoSlider
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense, lazy } from "react";
-import Layout from "@/components/Layout";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer/Footer";
-import VideoSlider from "@/components/Slider/VideoSlider";
-import Vidd from "@/components/Video";
+import React, { Suspense, lazy, useRef } from "react"
+import Layout from "@/components/Layout"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer/Footer"
+import VideoSlider from "@/components/Slider/VideoSlider"
+import Vidd from "@/components/Video"
+import {
+  Video,
+  Image,
+  CloudinaryContext,
+  Transformation,
+} from "cloudinary-react"
 //import { SliderData } from "@/components/Slider/SliderData.js";
 
-import BannerSection from "../sections/LandingSection";
+import BannerSection from "../sections/LandingSection"
 
 const SliderData = [
   {
@@ -14,23 +20,44 @@ const SliderData = [
       "https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4",
   },
   {
-    source:
-      "CyberMonkeys/robert-murray-toCqTyxsT4Q-unsplash_qbofl1.jpg",
+    source: "CyberMonkeys/robert-murray-toCqTyxsT4Q-unsplash_qbofl1.jpg",
   },
   {
-    source:
-      "CyberMonkeys/gautier-salles-uffQnKuJ-hc-unsplash_fl7nsn.jpg",
+    source: "CyberMonkeys/gautier-salles-uffQnKuJ-hc-unsplash_fl7nsn.jpg",
   },
   {
-    source:
-      "CyberMonkeys/20100925_WBP504_ft3ywl.webp",
+    source: "CyberMonkeys/20100925_WBP504_ft3ywl.webp",
   },
-];
+]
 const Home = () => {
+  const videoRef = useRef()
   return (
     <Layout>
       <Navbar />
-      <VideoSlider slides={SliderData} />
+      <CloudinaryContext cloud_name="cybermonkeysllc">
+        <div>
+          <Video
+            publicId="CyberMonkeys/B1_zdvhe0"
+            autoplay="true"
+            sound="false"
+            loop="true"
+            className="w-screen"
+            innerRef={videoRef}
+          >
+            <Transformation effect="fade:500" />
+          </Video>
+        </div>
+      </CloudinaryContext>
+      <hr className="lineSeperator"></hr>
+      <CloudinaryContext cloud_name="cybermonkeysllc">
+        <div>
+          <Image
+            publicId="CyberMonkeys/SLIDER_PRINCIPAL_JPG_ejwrkr.jpg"
+            innerRef={videoRef}
+          ></Image>
+        </div>
+      </CloudinaryContext>
+      {/* <VideoSlider slides={SliderData} /> */}
       {/* <HomeSection /> */}
       <BannerSection />
       {/* <GlobeSection />
@@ -40,7 +67,7 @@ const Home = () => {
       <LatestNewsSection /> */}
       <Footer />
     </Layout>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
