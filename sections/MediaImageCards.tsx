a99f7969b0160690b0351ec75d202506af98036d
team <team@Pablos-iMac.local>
2022-04-18 13:38:36 -0700

    Media section with 12 images and videos

--- sections/MediaImageCards.tsx
+++ sections/MediaImageCards.tsx
@@ -4,17 +4,28 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
-import sampleImage from "../images/gemstone.jpg";
+import image1 from "../images/gemstone.jpg";
+import image2 from "../images/gemstone.jpg";
+import image3 from "../images/gemstone.jpg";
+import image4 from "../images/gemstone.jpg";
+import image5 from "../images/gemstone.jpg";
+import image6 from "../images/gemstone.jpg";
+import image7 from "../images/gemstone.jpg";
+import image8 from "../images/gemstone.jpg";
+import image9 from "../images/gemstone.jpg";
+import image10 from "../images/gemstone.jpg";
+import image11 from "../images/gemstone.jpg";
+import image12 from "../images/gemstone.jpg";
 import { CardActionArea } from "@mui/material";
 
 export default function MediaImageCard() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image1}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -32,7 +43,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image2}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -50,7 +61,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image3}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -68,7 +79,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image4}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -86,7 +97,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image5}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -104,7 +115,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image6}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -122,7 +133,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image7}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -140,7 +151,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image8}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -157,25 +168,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
-              height={240}
-              width={345}
-              alt="CyberMonkeys Image"
-            />
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Photography
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia>
-            <Image
-              src={sampleImage}
+              src={image9}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -193,7 +186,7 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image10}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
@@ -211,7 +204,25 @@ export default function MediaImageCard() {
         <CardActionArea>
           <CardMedia>
             <Image
-              src={sampleImage}
+              src={image11}
+              height={240}
+              width={345}
+              alt="CyberMonkeys Image"
+            />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Photography
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia>
+            <Image
+              src={image12}
               height={240}
               width={345}
               alt="CyberMonkeys Image"
--- sections/MediaVideoCards.tsx
+++ sections/MediaVideoCards.tsx
@@ -7,13 +7,13 @@ import { CardActionArea } from "@mui/material";
 
 export default function MediaVideoCard() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
       <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea >
+        <CardActionArea>
           <CardMedia
-            component="video"
             height="140"
-            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            component="video"
+            src="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
             controls
           />
           <CardContent>
@@ -27,10 +27,10 @@ export default function MediaVideoCard() {
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
-             component="video"
-             height="140"
-             image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
-             controls
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
@@ -43,11 +43,10 @@ export default function MediaVideoCard() {
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
-             component="video"
-             height="140"
-             image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
-             controls
-            
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
@@ -60,10 +59,10 @@ export default function MediaVideoCard() {
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
-             component="video"
-             height="140"
-             image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
-             controls
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
@@ -76,10 +75,10 @@ export default function MediaVideoCard() {
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
-             component="video"
-             height="140"
-             image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
-             controls
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
@@ -92,10 +91,106 @@ export default function MediaVideoCard() {
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
-             component="video"
-             height="140"
-             image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
-             controls
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            height="140"
+            component="video"
+            src="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Video
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="video"
+            height="140"
+            image="https://res.cloudinary.com/cybermonkeysllc/video/upload/v1647887060/CyberMonkeys/B1_zdvhe0.mp4"
+            controls
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
